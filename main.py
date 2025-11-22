0d6e2a3269d53a3318913436d61c01c5c22114fe
mi-cawber <waltwest@outlook.com>
2025-11-21 20:36:31 -0500

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+up = np.array([0,1])
+down = np.array([0,-1])
+left = np.array([-1,0])
+right = np.array([1,0])
+
+direction = [up, down, left, right]
+
+position = np.array([0,0])
